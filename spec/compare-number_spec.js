7b06ec41ce68e291861d2166abadf76757252400
Seaborn Lee <seabornlee.cn@gmail.com>
2016-07-26 17:11:36 +0800

    Add more examples

--- spec/compare-number_spec.js
+++ spec/compare-number_spec.js
@@ -3,17 +3,29 @@ const CompareNumber = require('../src/compare-number');
 describe('Compare Number', () => {
     it('returns xAxB', () => {
         [{
-            input: '1234',
-            answer: '5678',
+            input: '5678',
+            answer: '1234',
             result: '0A0B'
         }, {
             input: '1234',
             answer: '1234',
             result: '4A0B'
         }, {
-            input: '1234',
-            answer: '1243',
+            input: '1243',
+            answer: '1234',
             result: '2A2B'
+        }, {
+            input: '5643',
+            answer: '1234',
+            result: '0A2B'
+        }, {
+            input: '4321',
+            answer: '1234',
+            result: '0A4B'
+        }, {
+            input: '1253',
+            answer: '1234',
+            result: '2A1B'
         }].forEach(example => {
             const result = CompareNumber.compare(example.input, example.answer);
             expect(result).toEqual(example.result);
